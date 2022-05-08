02e3e6c2d4fb3eccf72af85075d6f2e9603c931b
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-05-08 01:24:17 -0400

    Creacion de contenedor inical y texto basico

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, Image} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    //View : Contenedor .
+    <View style={styles.containerHome}>
+      <Image
+      style={styles.logo}
+      source={require('./assets/Logotipo.png')}
+      /> 
+      <Text style={styles.title}>Hola Mundo !!</Text>
     </View>
   );
-}
+};
+//Instancia que contiene todos los estilos.
+const styles =StyleSheet.create({
+  containerHome: {
+    flex:1,
+    backgroundColor:"#073c91",
+    justifyContent:"center",
+    alignContent:"center",
+    alignItems:"center"
+  },
+  title : {
+    fontSize:40
+  },
+  logo: {
+    width: 200,
+    height: 70
+  },
+})
