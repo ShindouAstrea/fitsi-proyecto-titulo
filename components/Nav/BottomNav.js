94c8a672eb444ab8fa3662d6b864566a80c81c60
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-12 01:08:56 -0400

    Nueva ruta y renombre de barra de navegacion principal

---
+++ components/Nav/BottomNav.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { View, StyleSheet,Button,Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ExampleScreen from '../ExampleScreen';
+import PlanningScreen from '../PlanningScreen';
+import AccountScreen from '../AccountScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
+function BottomNav({navigation}) {
+    return (
+    <Tab.Navigator>
+      
+      <Tab.Screen name="Account" component={AccountScreen} 
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Planning" component={PlanningScreen} 
+      options={{
+          tabBarLabel: 'Plan de ejercicio',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="dumbbell" color={color} size={26} />
+          ),
+        }}/>
+    </Tab.Navigator>
+    );
+  };
+  export default BottomNav;
+
+  const styles =StyleSheet.create({
+    containerHome: {
+      flex:1,
+      justifyContent:"center",
+      alignContent:"center",
+      alignItems:"center"
+    },
+    title : {
+      fontSize:40
+    },
+    logo: {
+      width: 200,
+      height: 70
+    },
+  })
