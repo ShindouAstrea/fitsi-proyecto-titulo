74823c139dbf6d50df09105d10e57c638a0dfa4d
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-15 16:43:43 -0400

    Cambios menores en el algoritmo

--- components/Algorithm/Algorithm.js
+++ components/Algorithm/Algorithm.js
@@ -1,9 +1,19 @@
 import * as React from 'react';
 import {Text, StyleSheet, View, Button} from 'react-native';
+import Accelerometer from '../Sensors/Accelerometer'
+
 
 function Algorithm(aceX,aceY,acZ){
   const resultado =aceX * aceY * acZ
   return resultado;
+  //Recibir controles del sensor
+  //Suscribe al sensor
+  //Definir tiempo de Update de suscription
+  //Correr reloj o tiempo de conteo de toma de datos
+  //detener por un tiempo especifico la toma de examen.
+  //Manjear un vector con datos de los ejes de un sensor
+  //Realizar algun calculo con la magnitud del vector y el tiempo fina de la captura de datos.
+
 
 };
 export default Algorithm ;
