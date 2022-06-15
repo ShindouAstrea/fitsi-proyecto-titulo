94eca549ee0fb17f90044b074a486cc282216e43
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-15 16:42:54 -0400

    Array con datos de ejercicios

--- components/Algorithm/Data.js
+++ components/Algorithm/Data.js
@@ -1,5 +1,35 @@
-const Data = () =>{
+const Data =[
     {
-        "name"="aaa"
-    }
-}
+        id:"01",
+        Nombre:"Flexiones",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+    {
+        id:"02",
+        Nombre:"Sentadillas",
+        Repeticiones:20,
+        Tiempo:30,
+        Video:"https://www.youtube.com/watch?v=l7aszLSPCVg",
+
+    },
+    {
+        id:"03",
+        Nombre:"Abdominales",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+    {
+        id:"04",
+        Nombre:"Press banca",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+];
+export default Data;
