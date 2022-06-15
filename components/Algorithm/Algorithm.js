8a2acdbaa0d6c82e41f3b866efb7b9782d2c671c
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-15 16:42:10 -0400

    Cambios en algoritmo ,falta diseño y pulir algoritmo de manipulacion de sensores

---
+++ components/Algorithm/Algorithm.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import {Text, StyleSheet, View, Button} from 'react-native';
+
+function Algorithm(aceX,aceY,acZ){
+  const resultado =aceX * aceY * acZ
+  return resultado;
+
+};
+export default Algorithm ;
