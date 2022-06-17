39871898b4330bb3111bd5fea8e3f341137e6cb8
Nicolas Gonzalez Marin <Nicku.gonzalez@gmail.com>
2022-06-17 05:58:27 -0400

    cambio a App.js

--- App.js
+++ App.js
@@ -4,16 +4,23 @@ import { View, Text,Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
  //JS que contienen las funciones o "screens" de las diversas pantallas.
- import SignupScreen from './components/SignupScreen';
+ 
+import SignupScreen from './components/SignupScreen';
+//import SignupScreen2 from './components/SignupScreen2'; No se usan
+//import SignupScreen3 from './components/SingupScreen3'; No se usan
+
+
 import BottomNav from './components/Nav/BottomNav';
+
 //JS que contienen las vistas de prueba de sensores
 import Accelerometer from './components/Sensors/Accelerometer';
 import Gyroscope from './components/Sensors/Gyroscope';
 //JS  que contienen las vistas relacionadas al plan de ejercicio
 import AccountScreen from './components/AccountScreen';
-import ExampleScreen from './components/ExampleScreen';
+import DetailExerciseScreen from './components/DetailExerciseScreen';
 import PlanningScreen from './components/PlanningScreen';
-//import Ejemplo from './components/Ejemplo';
+import Ejemplo from './components/Ejemplo';
+import Algorithm from './components/Algorithm/Algorithm';
 
 
 
@@ -40,25 +47,25 @@ const App = () => {
           headerStyle: {
             backgroundColor: '#4cbdfd',
           },
-          headerTintColor: '#ffffff',
+          headerTintColor: '#4cbdfd',
           headerTitleStyle: {
-            justifyContent: "center",
-            alignContent: "center",
-            alignItems: "center",
             fontWeight: 'normal',
+            backgroundColor: '#4cbdfd',
           },
         }}
     >
         <Stack.Screen name="Home" component={BottomNav} 
-        options={{headerShown: true}} />
+        options={{headerShown: false}} />
         <Stack.Screen name="Gyroscope" component={Gyroscope} 
         options={{headerShown: true , title: "Giroscopio pruebas"}} />
         <Stack.Screen name="Accelerometer" component={Accelerometer}  
         options={{headerShown: true,title: 'Acelerometro pruebas'}} />
         <Stack.Screen name="Forms" component={SignupScreen} options={{title: 'Registro' }}/>
         <Stack.Screen name="Account" component={AccountScreen} options={{title: 'Perfil' }}/>
-        <Stack.Screen name="Example" component={ExampleScreen} options={{title: 'Como realizarlo' }}/>
+        <Stack.Screen name="Details" component={DetailExerciseScreen} options={{title: 'Planes de Ejercicio' }}/>
         <Stack.Screen name="Planning" component={PlanningScreen} options={{title: 'Plan de ejercicio' }}/>
+        <Stack.Screen name="Algorithm" component={Algorithm}/>
+        <Stack.Screen name="Ejemplo" component={Ejemplo}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
