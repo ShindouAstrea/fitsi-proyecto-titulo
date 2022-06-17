eee6b56e3b3ea5bf949c69424f9dbdff89bcb511
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-17 06:19:11 -0400

    Se implementa enlistado

--- components/Algorithm/ExercisesPlans.js
+++ components/Algorithm/ExercisesPlans.js
@@ -2,6 +2,7 @@ const Plans =[
     {
         id:"01",
         title:"Aumento de Fuerza",
+        dificultad:"0",
         descripcion:"Este plan consiste en ejercicio dedicatos al aumentos de tu masa muscular",
         picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
   
@@ -9,6 +10,7 @@ const Plans =[
     {
         id:"02",
         title:"Aumento de Resistencia",
+        dificultad:"0",
         descripcion:"Este plan consiste en ejercicios (en su mayoria anaerobicos) para aumentar la resistencia fisica y capacidad de oxigenacion de tu cuerpo. ",
         picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
   
@@ -16,6 +18,7 @@ const Plans =[
     {
       id:"03",
       title:"Aumento de piernas",
+      dificultad:"0",
       descripcion:"Este plan consiste en ejercicio dedicatos. ",
       picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
   
@@ -23,6 +26,7 @@ const Plans =[
     {
         id:"04",
         title:"Aumento de cerebro",
+        dificultad:"5",
         descripcion:"Este plan consiste en ejercicios mentales. ",
         picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
   
@@ -30,6 +34,7 @@ const Plans =[
     {
       id:"05",
       title:"Aumento de brazos",
+      dificultad:"0",
       descripcion:"Este plan consiste en ejercicios de brazos",
       picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
   
@@ -37,6 +42,7 @@ const Plans =[
     {
       id:"06",
       title:"Aumento de Estatura",
+      dificultad:"5",
       descripcion:"Este plan consiste en ejercicios de estiramiento . ",
       picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
   
