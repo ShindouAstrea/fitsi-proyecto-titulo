920ead8940787e6febea27451e88fe8a1ff61ece
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-23 00:20:36 -0400

    Cambios menores

--- components/Algorithm/ExercisesPlans.js
+++ components/Algorithm/ExercisesPlans.js
@@ -1,7 +1,7 @@
 const Plans =[
     {
         id:"01",
-        title:"Aumento de Fuerza",
+        titulo:"Aumento de Fuerza",
         dificultad:"0",
         descripcion:"Este plan consiste en ejercicio dedicatos al aumentos de tu masa muscular",
         picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
@@ -9,7 +9,7 @@ const Plans =[
     },
     {
         id:"02",
-        title:"Aumento de Resistencia",
+        titulo:"Aumento de Resistencia",
         dificultad:"0",
         descripcion:"Este plan consiste en ejercicios (en su mayoria anaerobicos) para aumentar la resistencia fisica y capacidad de oxigenacion de tu cuerpo. ",
         picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
@@ -17,15 +17,15 @@ const Plans =[
     },
     {
       id:"03",
-      title:"Aumento de piernas",
-      dificultad:"0",
+      titulo:"Aumento de piernas",
+      dificultad:"1",
       descripcion:"Este plan consiste en ejercicio dedicatos. ",
       picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
   
     },
     {
         id:"04",
-        title:"Aumento de cerebro",
+        titulo:"Aumento de cerebro",
         dificultad:"5",
         descripcion:"Este plan consiste en ejercicios mentales. ",
         picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
@@ -33,16 +33,16 @@ const Plans =[
     },
     {
       id:"05",
-      title:"Aumento de brazos",
-      dificultad:"0",
+      titulo:"Aumento de brazos",
+      dificultad:"4",
       descripcion:"Este plan consiste en ejercicios de brazos",
       picture:"https://sp.depositphotos.com/10171107/stock-photo-strong-biceps.html",
   
     },
     {
       id:"06",
-      title:"Aumento de Estatura",
-      dificultad:"5",
+      titulo:"Aumento de Estatura",
+      dificultad:"3",
       descripcion:"Este plan consiste en ejercicios de estiramiento . ",
       picture:"https://sp.depositphotos.com/132428518/stock-photo-woman-trail-runner-running-on.html",
   
