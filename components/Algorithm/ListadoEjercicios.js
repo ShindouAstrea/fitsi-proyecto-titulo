a1877c8c91c33113ece863cf4dbc7022f8a5d214
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-06-23 00:19:00 -0400

    Cambios menores

---
+++ components/Algorithm/ListadoEjercicios.js
@@ -0,0 +1,62 @@
+
+
+const ListadoEjercicios =[
+    {
+        id:"01",
+        Nombre:"Flexiones",
+        Dificultad:"5",
+        Tipo:"{3},{2},{1}",
+        Series:"2",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+    {
+
+        id:"02",
+        Nombre:"Sentadillas",
+        Dificultad:"0",
+        Tipo:"{3},{2},{1}",
+        Series:"1",
+        Repeticiones:20,
+        Tiempo:30,
+        Video:"https://www.youtube.com/watch?v=l7aszLSPCVg",
+
+    },
+    {
+        id:"03",
+        Nombre:"Abdominales",
+        Dificultad:"0",
+        Tipo:"{3},{2},{1}",
+        Series:"2",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+    {
+        
+        id:"04",
+        Nombre:"Press banca",
+        Dificultad:"5",
+        Tipo:"{3},{2},{1}",
+        Series:"1",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=e_EKkqoHxns",
+
+    },
+    {
+        id:"05",
+        Nombre:"Prueba video",
+        Dificultad:"5",
+        Tipo:"{3},{2},{1}",
+        Series:"1",
+        Repeticiones:10,
+        Tiempo:20,
+        Video:"https://www.youtube.com/watch?v=mZHSimF0TkY&ab_channel=RafaelCaldera",
+    },
+];
+export default ListadoEjercicios;
+
