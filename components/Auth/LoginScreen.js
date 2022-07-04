14996ee6d69b08df75070aad3c532bef6f5f7b38
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-07-03 20:51:59 -0400

    Se implementa Inicio de Sesion

--- components/Auth/LoginScreen.js
+++ components/Auth/LoginScreen.js
@@ -1,34 +1,66 @@
 import  React, {useState} from 'react';
 import {View, Button,Text,TextInput,StyleSheet,StatusBar,} from 'react-native';
+//Imports Librerias Firebase
+import {getAuth, signInWithEmailAndPassword,sendPasswordResetEmail} from 'firebase/auth';
+import {firebaseConfig} from '../../database/firebase';
+import { initializeApp } from "firebase/app";
+//----------------------------------------------------------------
 import FlatButton from '../Buttom';
-
+ // Constantes para conectar con la base de datos de firebase
+ const app  = initializeApp(firebaseConfig);
+ const auth = getAuth(app);
+ //----------------------------------------------------------------
 const Separator = () => <View style={styles.separator} />;
 function LoginScreen({navigation}) {
-  const [nombre, changeName] = useState(null); //Variable creada para que el nombre de lo usuarios
-  const [password, changePassword] = useState(null);
-  const [state,setState] = useState(null); //Variable creada para capturar si es que esta logueado.
+  const [mail, setMail] = useState(null); //Variable creada para que el nombre de lo usuarios
+  const [password, setPass] = useState(null);
+  const recuperarPass=()=>{
+    sendPasswordResetEmail(auth, mail);
+  }
+  const iniciarSesion=()=>{
+    signInWithEmailAndPassword(auth,mail,password)
+    .then((userCredential)=>{
+      console.log("sesion iniciada");
+      const user = userCredential.user ;
+    })
+    .catch(error =>{
+      alert("Ya existe una cuenta con este usuario registrado")
+    })
+   }
   
     return (
       <View style={styles.container}>
-        <Text styles={styles.title}> Ingrese Nombre</Text>
+        <Text styles={styles.title}> Ingrese Correo registrado</Text>
         <TextInput
           style={styles.input}
-          onChangeText={changeName}
-          value={nombre}
-          placeholder="Ingrese Nombre" />
+          onChangeText={(text)=>setMail(text)}
+          value={mail}
+          placeholder=" Ingrese Mail" />
         <Text styles={styles.title}> Ingrese Contraseña</Text>
         <TextInput
           style={styles.input}
-          onChangeText={changePassword}
+          onChangeText={(text)=>setPass(text)}
           value={password}
-          placeholder="Ingrese Contraseña" />
-        
-          <Separator/>
+          placeholder=" Ingrese Contraseña" 
+          secureTextEntry={true}/>
           
           <Separator/>
            <FlatButton
               text="Iniciar Sesión "
-              onPress={() => navigation.navigate('Account')}
+              onPress={() => {
+                iniciarSesion();
+                navigation.navigate('Account')}
+              }
+          />
+           <Separator/>
+          <FlatButton
+              text="Olvidé Mi contraseña "
+              onPress={() => {
+                recuperarPass();
+                alert("Por favor revisa tu correo (puede estar en carpeta SPAM)");
+              }}
+
+              
           />
         </View>
       );
@@ -44,9 +76,6 @@ const styles = StyleSheet.create({
         marginVertical: 8,
     
       },
-    button:{
-      marginVertical: 10,
-    },
     input: {
       height: 40,
       margin: 12,
@@ -54,6 +83,8 @@ const styles = StyleSheet.create({
       padding: 10,
     },
     title: {
-  
+        fontStyle: 'italic',
+        fontSize:30,
+        fontWeight: 'bold',
     }
   });
