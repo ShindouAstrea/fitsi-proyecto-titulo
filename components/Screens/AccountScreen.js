8740c99791007d83b9a07d9f796d8c214b5b0fb2
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-07-04 06:42:43 -0400

    Muestra de datos

--- components/Screens/AccountScreen.js
+++ components/Screens/AccountScreen.js
@@ -1,5 +1,5 @@
   import * as React from 'react';
-import { View, Text,Button ,StyleSheet} from 'react-native';
+import { View, Text,FlatList ,StyleSheet} from 'react-native';
 import FlatButton from '../Buttom';
 import Userinfo from '../Userinfo';
 import { initializeApp } from 'firebase/app';
@@ -12,29 +12,67 @@ const auth = getAuth(app);
 const db= getFirestore(app);
 const Separator = () => <View style={styles.separator} />;
 
+    /**
+     * Funcion para crear un item u objeto para mostrarse en forma de View
+     * @param {string} title- Valor del que tiene "title" en el array entregado a FlatList.
+     * @param {string} description - Valor que tiene "description" en el array entregado a FlatList.
+     * @returns  {View} [Objeto en una vista , similar a un card]
+     */
+
+     const Item = ({ correo,nombre,apellido,cuerpo,genero,edad,imc,peso,apodo,silueta,altura}) => (
+      <View style={styles.item}>
+        <Text style={styles.description}>Correo registrado: {correo}</Text>
+        <Text style={styles.description}>Mi nombre: {nombre}</Text>
+        <Text style={styles.description}>Mi Apellido: {apellido}</Text>
+        <Text style={styles.description}>Mi apodo: {apodo}</Text>
+        <Text style={styles.description}>Mi Edad: {edad}</Text>
+        <Text style={styles.description}>Mi altura: {altura}</Text>
+        <Text style={styles.description}>Mi imc: {imc}</Text>
+        <Text style={styles.description}>Mi silueta: {silueta}</Text>
+        <Text style={styles.description}>Mi altura: {altura}</Text>
+        <Text style={styles.description}>Mi genero: {genero}</Text>
+        <Text style={styles.description}>Mi Peso: {peso}</Text>
+      </View>
+    )
+let correoUser ;
+
 function AccountScreen({navigation}) {
   const{signOut} = React.useContext(AuthContext) ;
-  let nombre ;
-  let email ;
   const user = auth.currentUser;
   if(user){
-    email = user.email;
+    correoUser=user.email;
   }
-  const [usuario,setUsers]=React.useState([]);
-
+  const [listaUsuarios,setUsers]=React.useState([]);
   async function cargardatos () {
-    const a = await getDoc(doc(db,"Usuarios", Correo));
-    setUsers(a.data());
-    console.log(a);
-  }
+    const Usuarios =  await getDocs(collection(db,'Usuarios'));
+    setUsers(Usuarios.docs);
+  };
   React.useEffect(() => {
     cargardatos();
   },[])
+  /**
+   * 
+   * @param {item} Objeto - Objeto que se desea renderizar muchas veces en una lista 
+   * @returns {View} Listado de items
+   */
+   const renderItem = ({ item }) => (
+    
+    <Item correo={item.data().Correo} edad={item.data().Edad}nombre ={item.data().Nombre} apellido={item.data().Apellido} cuerpo={item.data().Cuerpo} genero={item.data().Genero} imc={item.data().IMC} peso={item.data().Peso} silueta={item.data().Silueta} apodo={item.data().Apodo} altura={item.data().Altura}
+    
+    />
+  )
   return (
 
       <View style={styles.container}>
         <Separator/>
-      <Text style={styles.title}>Bienvenido: {email}</Text>
+      <Text style={styles.title}>Mis datos </Text>
+      <FlatList
+            //data={Plans.filter(Plans=>{return Plans.dificultad == Nivel ;})}
+            data={listaUsuarios.filter(listaUsuarios=>{return listaUsuarios.data().Correo == correoUser})}
+            keyExtractor={item => item.id}
+            renderItem={renderItem}
+            extraData={navigation}
+          />
       <Separator/>
         <FlatButton 
             text="Modificar datos"
@@ -65,10 +103,13 @@ const styles = StyleSheet.create({
   button:{
     borderRadius:10 
   },
+  description:{
+    fontSize: 18
+  },
   title: {
     textAlign: 'center',
     marginVertical: 8,
-    fontSize:20
+    fontSize:16
   },
   fixToText: {
     flexDirection: 'row',
