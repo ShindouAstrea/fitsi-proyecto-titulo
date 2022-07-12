2fa0326cceaf5168d1894d11fed7ff28821228c7
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-07-12 01:25:57 -0400

    Cambios menores

--- components/Buttom.js
+++ components/Buttom.js
@@ -6,7 +6,9 @@ export default function FlatButton({text,onPress}){
       <TouchableOpacity onPress={onPress}>
        <View style={styles.button}>
           <Text style={styles.buttonText}>{text}</Text>
+
        </View>
+       
       </TouchableOpacity>
     )
 }   
