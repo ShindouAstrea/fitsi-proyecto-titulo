cecbd04ae4fe994dbf72303c037081b82f9683c5
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-07-20 03:35:50 -0400

    js para factorizar auth

---
+++ aaaa.js
@@ -0,0 +1,65 @@
+import { createContext, useContext,useMemo,useReducer} from "react";
+
+const AuthContext = createContext();
+export const UseAuth =()=>{
+    return useContext(AuthContext)
+};
+export const [state, dispatch] = useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+const authContext = useMemo(
+    () => ({
+      signIn: async (data) => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `SecureStore`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async (data) => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `SecureStore`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+  const AuthProvider =({children}) =>{
+    return (
+        <AuthContext.Provider value={authContext}>
+            {children}
+        </AuthContext.Provider>
+    )
+  }
+  export default AuthProvider;
